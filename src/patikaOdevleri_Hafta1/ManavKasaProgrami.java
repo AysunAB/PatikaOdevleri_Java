45623f5e70e8ccd38c2a09bfa8222a08c16faf6d
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-02 08:12:40 +0300

    02.11.22-08.12

---
+++ src/patikaOdevleri_Hafta1/ManavKasaProgrami.java
@@ -0,0 +1,56 @@
+package patikaOdevleri_Hafta1;
+
+import java.util.Scanner;
+
+public class ManavKasaProgrami {
+    /*Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+
+Meyveler ve KG Fiyatları
+
+Armut : 2,14 TL
+Elma : 3,67 TL
+Domates : 1,11 TL
+Muz: 0,95 TL
+Patlıcan : 5,00 TL
+Örnek Çıktı;
+
+Armut Kaç Kilo ? :0
+Elma Kaç Kilo ? :1
+Domates Kaç Kilo ? :1
+Muz Kaç Kilo ? :2
+Patlıcan Kaç Kilo ? :3
+Toplam Tutar : 21.68 TL*/
+
+    /**
+     * @author aysun
+     */
+
+    public static void main(String[] args) {
+
+        double armut,elma,domates,muz,patlican,armutKilo,elmaKilo,domatesKilo,muzKilo,patlicanKilo,toplamTutar;
+
+        armut=12.14;
+        elma=13.67;
+        domates=11.15;
+        muz=21.33;
+        patlican=34.5;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kac kilo armut istersiniz : ");
+        armutKilo= scan.nextDouble();
+        System.out.print("Kac kilo elma istersiniz : ");
+        elmaKilo= scan.nextDouble();
+        System.out.print("Kac kilo domates istersiniz : ");
+        domatesKilo= scan.nextDouble();
+        System.out.print("Kac kilo muz istersiniz : ");
+        muzKilo= scan.nextDouble();
+        System.out.print("Kac kilo patlican istersiniz : ");
+        patlicanKilo= scan.nextDouble();
+
+        toplamTutar=(armut*armutKilo)+(elma*elmaKilo)+(domates*domatesKilo)+(muz*muzKilo)+(patlican*patlicanKilo);
+        System.out.print("Toplam tutar :"+ toplamTutar);
+
+
+
+    }
+}
