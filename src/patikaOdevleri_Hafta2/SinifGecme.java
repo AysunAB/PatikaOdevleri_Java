7286f150a70e52910a88322925b5584f832fa124
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-08 04:43:13 +0300

    08.11.22-04.40

---
+++ src/patikaOdevleri_Hafta2/SinifGecme.java
@@ -0,0 +1,10 @@
+package patikaOdevleri_Hafta2;
+
+public class SinifGecme {
+    /*Dersler : Matematik, Fizik, Türkçe, Kimya, Müzik
+
+    Geçme Notu : 55
+
+    Ödev
+    Eğer girilen ders notları 0 veya 100 arasında değil ise ortalamaya katılmasın.*/
+}
