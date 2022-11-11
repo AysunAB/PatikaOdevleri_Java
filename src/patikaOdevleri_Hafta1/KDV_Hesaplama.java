9ae6c152022df5182d16c664c483e0eae989e276
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-11 20:34:00 +0300

    11.11.22-20.33

--- src/patikaOdevleri_Hafta1/KDV_Hesaplama.java
+++ src/patikaOdevleri_Hafta1/KDV_Hesaplama.java
@@ -9,21 +9,22 @@ public class KDV_Hesaplama {
      */
 
     /*
-    Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve
-     KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+             Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve
+              KDV tutarını hesaplayıp ekrana bastıran programı yazın.
 
-(Not : KDV tutarını 18% olarak alın)
+         (Not : KDV tutarını 18% olarak alın)
 
-KDV'siz Fiyat = 10;
+         KDV'siz Fiyat = 10;
 
-KDV'li Fiyat = 11.8;
+         KDV'li Fiyat = 11.8;
 
-KDV tutarı = 1.8;
+         KDV tutarı = 1.8;
 
-Ödev
-Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
-tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
-     */
+         Ödev
+         Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+         tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+
+              */
 
     public static void main(String[] args) {
 
