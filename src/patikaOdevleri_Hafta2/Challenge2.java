7e6725ac341c97d9d2730623c139da1c3d8620dc
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-11 05:39:12 +0300

    11.11.22-05.35

--- src/patikaOdevleri_Hafta2/Challenge2.java
+++ src/patikaOdevleri_Hafta2/Challenge2.java
@@ -7,22 +7,24 @@ public class Challenge2 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Write a number");
         int n = scan.nextInt();
 
 
-        if (n % 2 != 0) {
+        if (n % 2 == 1) {
             System.out.print("Weird");
         } else {
 
-            if (n >= 2 && n <= 5 && n > 20) {
+            if (n >= 2 && n <= 5) {
                 System.out.print("Not Weird");
             }
 
-            if (n >= 6 && n<= 20) {
+            if (n >= 6 && n <= 20) {
                 System.out.print("Weird");
             }
+            if (n > 20) {
+                System.out.print("Not Weird");
 
+            }
         }
     }
 }
