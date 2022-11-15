98e41faf2574aaeaa6dfd26015dff6bb438d632a
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-16 00:06:16 +0300

    15.11.2218-00.05

--- src/patikaOdevleri_Hafta3/FibonacciSerisi.java
+++ src/patikaOdevleri_Hafta3/FibonacciSerisi.java
@@ -14,10 +14,29 @@ public class FibonacciSerisi {
  Elde edilen sonuç rakamın sağ tarafına yazılır. Fibonacci dizisinin ilk on sayısı şu şekildedir:
  9 Elemanlı Fibonacci Serisi : 0 1 1 2 3 5 8 13 21 34 */
 
+        public static void main(String[] args) {
+                int x = 0,y=1,z,t;
 
 
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Fibonacci sersinin eleman sayisini yaziniz: ");
+                t = scan.nextInt();
+                System.out.print(x+", "+y);
 
+                for (int i = 2; i <= t; i++) {
+                        z = x+y;
+                        System.out.print(", "+z);
+                        x = y;
+                        y = z;
+                        System.out.print(" ");
+                }
         }
+        }
+
+
+
+
+
 
 
 
