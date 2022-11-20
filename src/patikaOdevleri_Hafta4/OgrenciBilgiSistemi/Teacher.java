54df8b0bae6cd432cc26316448b56b3a40cbdbc4
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-20 05:58:30 +0300

    20.11.22

---
+++ src/patikaOdevleri_Hafta4/OgrenciBilgiSistemi/Teacher.java
@@ -0,0 +1,13 @@
+package patikaOdevleri_Hafta4.OgrenciBilgiSistemi;
+
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
