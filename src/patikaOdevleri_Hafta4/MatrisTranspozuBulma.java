9e9c44c438a6d652701c9a097b3557fdc5291663
Aysun Arıcı Berkil <aysunariciberkil@gmail.com>
2022-11-21 09:16:40 +0300

    20.11.22-v1

--- src/patikaOdevleri_Hafta4/MatrisTranspozuBulma.java
+++ src/patikaOdevleri_Hafta4/MatrisTranspozuBulma.java
@@ -8,47 +8,33 @@ public class MatrisTranspozuBulma {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Matrisin satır sayısını giriniz : ");
-        int row = scanner.nextInt();
-        System.out.print("Matrisin sütun sayısını giriniz : ");
-        int column = scanner.nextInt();
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Matrisin satir sayisini giriniz : ");
+        int row = scan.nextInt();
+        System.out.print("Matrisin sutun sayisini giriniz : ");
+        int column = scan.nextInt();
         int[][] matris = new int[row][column];
-        int[][] transpoze = new int[column][row];
-
+        System.out.print("\n");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                System.out.print((i + 1) + ". satır " + (j + 1) + ". sütun : ");
-                matris[i][j] = scanner.nextInt();
+                System.out.print((i + 1) + ". satir - " + (j + 1) + ". sutun : ");
+                matris[i][j] = scan.nextInt();
             }
+            System.out.print("\n");
         }
-
-        System.out.println();
-
-        System.out.println("Matris : ");
-        System.out.println();
-
+        System.out.println("Matris :");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                System.out.print(matris[i][j] + " ");
+                System.out.print("\t" + matris[i][j]);
             }
-            System.out.println(" ");
+            System.out.print("\n");
         }
-
-        System.out.println();
-
-        System.out.println("Transpoze : ");
-        System.out.println();
-
-        for (int i = 0; i < column; i++) {
-            for (int j = 0; j < row; j++) {
-                transpoze[i][j] = matris[j][i];
-                System.out.print(transpoze[i][j] + " ");
+        System.out.println("Transpoze :");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= column; j++) {
+                System.out.print("\t" + matris[j][i]);
             }
-            System.out.println(" ");
+            System.out.print("\n");
         }
-
     }
 }
